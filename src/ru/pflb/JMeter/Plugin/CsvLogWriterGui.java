1b57eb4511db87b5d593da739e4a1eb5a1593c8e
Your Name <you@example.com>
2016-01-30 00:20:00 +0400

    для отладки CsvLogWriterGui добавлено ведение debug-лога. Работает при настройке log_level.ru.pflb=DEBUG в файле user.properties. Реазован минимальная функциональность по отображению компонента.

--- src/ru/pflb/JMeter/Plugin/CsvLogWriterGui.java
+++ src/ru/pflb/JMeter/Plugin/CsvLogWriterGui.java
@@ -20,30 +20,69 @@ import javax.swing.JTextField;
 
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.visualizers.gui.AbstractListenerGui;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class CsvLogWriterGui extends AbstractListenerGui {
+
+    private static final Logger log = LoggingManager.getLoggerForClass();
 
     //JMeterGUIComponent.getLabelResource()
     @Override
     public String getLabelResource()
     {
-        throw new NotImplementedException();
+        if (log.isDebugEnabled()) { log.debug("CsvLogWriterGui.getLabelResource()");}
+
+        return "CsvLogWriterGui_displayName";
     }
 
     //JMeterGUIComponent.createTestElement()
     @Override
-    public TestElement createTestElement()
-    {
-        throw new NotImplementedException();
+    public TestElement createTestElement(){
+        if (log.isDebugEnabled()) { log.debug("CsvLogWriterGui.createTestElement()");}
+        TestElement te = new CsvLogWriter();
+        modifyTestElement(te);
+        te.setComment("comment");
+        te.setName("pflb@CsvLogWriter");
+        return te;
     }
 
     //JMeterGUIComponent.modifyTestElement(TestElement te)
     @Override
     public void modifyTestElement(TestElement te)
     {
+        super.configureTestElement(te);
+        if (log.isDebugEnabled()) { log.debug("CsvLogWriterGui.modifyTestElement( TestElement te = " + te + " )"); }
+        if (te instanceof CsvLogWriter) {
 
+        }
     }
 
+    public CsvLogWriterGui()
+    {
+        super();
+        if (log.isDebugEnabled()) { log.debug("CsvLogWriterGui.CsvLogWriterGui()");}
+        init();
+    }
+
+    @Override
+    public String getStaticLabel() {
+        if (log.isDebugEnabled()) { log.debug("CsvLogWriterGui.getStaticLabel()");}
+        return "pflb@CsvLogWriter";
+    }
+
+    private void init() {
+        if (log.isDebugEnabled()) { log.debug("CsvLogWriterGui.init()");}
+        setLayout(new BorderLayout(0, 5));
+        setBorder(makeBorder());
+        add(makeTitlePanel(), BorderLayout.NORTH);
+
+        JPanel mainPanel = new JPanel(new GridBagLayout());
+
+        JPanel container = new JPanel(new BorderLayout());
+        container.add(mainPanel, BorderLayout.NORTH);
+        add(container, BorderLayout.CENTER);
+    }
 
 }
